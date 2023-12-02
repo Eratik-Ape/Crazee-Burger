dc9e71e19cb87a7081baa40fa8a66ca0b5d52130
Eratik-Ape <el.shogun91@gmail.com>
2023-12-02 19:42:55 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+
+const [inputValue, setInputValue] = useState("")
+
+
+    const handleSubmit = (e) => { 
+        e.preventDefault
+        alert(`Bonjour ${inputValue}`)
+        setInputValue("")
+     }
+
+     const handleChange = (e) => { 
+        setInputValue(e.target.value)
+      }
+
   return (
-    <div>LoginPage</div>
+    <div>
+        <h1>Bienvenue chez nous !</h1>
+        <br />
+        <h2>Connectez-vous</h2>
+        <form action="submit" onSubmit={handleSubmit}>
+            <input value={inputValue}
+            onChange={handleChange}
+            type="text" placeholder="Entrez votre prénom" required />
+            <button>Accéez à votre espace</button>
+        </form>
+    </div>
   )
 }
