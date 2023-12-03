93e0e8db4131a5484412fb829eba01af322d2452
Eratik-Ape <el.shogun91@gmail.com>
2023-12-03 22:56:59 +0100

    💄 F03: LoginPage > add layout

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react"
 import LoginForm from "./LoginForm"
+import styled from "styled-components";
+import Logo from "../reusable-ui/Logo";
+
 
 export default function LoginPage() {
 
   return (
-    <LoginForm />
+    <LoginPageStyled>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
   )
 }
+
+const LoginPageStyled = styled.div`
+  background: red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
