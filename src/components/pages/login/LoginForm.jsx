e9d552933af81cde01bc44b47ca0da32b8b61e8a
Eratik-Ape <el.shogun91@gmail.com>
2023-12-06 19:23:46 +0100

    💄 F03: LOGIN > LoginForm > button > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { BsPersonCircle } from 'react-icons/bs';
+import { IoChevronForward } from "react-icons/io5";
 
 
 export default function LoginForm() {
@@ -33,18 +34,21 @@ export default function LoginForm() {
           onChange={handleChange}
           type="text" placeholder="Entrez votre prénom" required />
         </div>
-          <button>Accédez à votre espace</button>
+          <button className='button-with-icon'>
+            <span>Accéder à mon espace</span>
+          <IoChevronForward className='icon' />
+          </button>
       </div>
     </LoginFormStyled>
   )
 }
 
-const LoginFormStyled = styled.div`
+const LoginFormStyled = styled.form`
   background: green;
   text-align: center;
-  min-width: 500px;
+  max-width: 500px;
   min-width: 400px;
-  margin: 0 auto;
+  margin: 0px auto;
   padding: 2.5rem 2rem;
   border-radius: 5px;
   font-family: "Amatic SC", cursive;
@@ -84,11 +88,58 @@ const LoginFormStyled = styled.div`
       border: none;
       font-size: 15px;
       color: #17161a;
+      /* width: 100%; */
     }
 
     &::placeholder {
       background: white;
       color: lightgrey;
+    }
+  }
+
+  .button-with-icon {
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: 1px solid #ff9f1b;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+      transition: all 200ms ease-out;
+    } 
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
     }
   }
 `
