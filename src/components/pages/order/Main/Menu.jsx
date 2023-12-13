21158626a00cdb3f4b0fb4c7b4d8cb8814e96fbc
Eratik-Ape <el.shogun91@gmail.com>
2023-12-13 20:53:50 +0100

    🍱 F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Menu() {
+
+  useState
   return (
     <MenuStyles className="menu">Menu</MenuStyles>
   )
