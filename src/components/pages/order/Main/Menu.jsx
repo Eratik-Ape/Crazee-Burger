9bc9ae3b018d635f43550903a55f646510ddf089
Eratik-Ape <el.shogun91@gmail.com>
2023-12-14 12:49:37 +0100

    ♻️ F05: ORDER > MENU > Move style to <Product/>

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -2,34 +2,22 @@ import { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { theme } from './../../../../theme/index';
+import Product from "./Product";
 
 export default function Menu() {
 
   const [products, setProducts] = useState(fakeMenu2)
 
   return (
-    <MenuStyles className="menu">
+    <MenuStyled className="menu">
       {products.map((p) => {
-        return (
-        <div className="produit">
-          <div className="image">
-            <img src={p.imageSource} alt={p.title} />
-          </div>
-          <div className="info-text">
-            <div className="title">{p.title}</div>
-            <div className="description">
-            <div className="price">{p.price}</div>
-            <button className="add-button">Ajouter</button>
-            </div>
-          </div>
-        </div>
-        )
+        return <Product title={p.title} imageSource={p.imageSource} price={p.price} />
       })}
-      </MenuStyles>
+      </MenuStyled>
   )
 }
 
-const MenuStyles = styled.div`
+const MenuStyled = styled.div`
     background: ${theme.colors.background_white};
     display: grid;
     grid-template-columns: repeat(4, 1fr);
@@ -37,20 +25,4 @@ const MenuStyles = styled.div`
     padding: 50px 50px 150px;
     justify-items: center;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-    .produit {
-      background: red;
-      width: 240px;
-      height: 330px;
-
-    .image {
-      border: 1px solid fuchsia;
-      width: 100px;
-      height: auto;
-      img {
-        width: 100%;
-        h100%
-      }
-    }
-  }
 `
