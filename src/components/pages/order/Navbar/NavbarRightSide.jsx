f69a2daa344c80cd46cf6465d7dfc31b050101e1
Eratik-Ape <el.shogun91@gmail.com>
2023-12-17 09:37:04 +0100

    ✨ F06: NAVBAR > Add toast notification on admin button

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,16 +1,41 @@
-import { Link } from "react-router-dom";
 import { styled } from 'styled-components';
 import Profile from "./Profile";
 import ToggleButton from "../../reusable-ui/ToggleButton";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { theme } from "../../../../theme";
+import { useState } from "react";
 
 export default function NavbarRightSide({username}) {
+
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setIsModeAdmin(!isModeAdmin)
+  }
+
   return (
     <NavbarRightSideStyled>
     {/* <div className="admin-button">Admin button</div> */}
     <ToggleButton
       labelIfUnchecked="Activer le mode admin"
-      labelIfChecked="Désactiver le mode admin" />
+      labelIfChecked="Désactiver le mode admin"
+      onToggle={displayToastNotification} />
     <Profile username={username} />
+    <ToastContainer className="toaster" bodyClassName="body-toast" />
    </NavbarRightSideStyled>
   )
 }
@@ -19,4 +44,22 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `
