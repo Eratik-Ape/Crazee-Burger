9107e7cf5170fc5dae5563b6fb62118c23e23ab0
Eratik-Ape <el.shogun91@gmail.com>
2023-12-20 18:55:18 +0100

    💄 F07/ MAIN > Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,22 +6,41 @@ export default function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
-      <Menu/>
+      <div className='menu-and-admin'>
+        <Menu/>
+        <div className="admin">Admin</div>
+      </div>
     </MainStyled>
   )
 }
 
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
-  flex: 1;
+  height: calc(95vh - 10vh);
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   display: grid;
   grid-template-columns: 1fr;
-  overflow-y: scroll;
 
   /* .basket {
     background: pink;
   } */
+
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+    .admin {
+      background: red;
+      height: 250px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+  }
 `
