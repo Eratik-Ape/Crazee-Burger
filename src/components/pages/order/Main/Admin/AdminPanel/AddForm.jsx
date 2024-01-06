472df4054f81632c6fb8961fa727e8a7a8ae4d61
Eratik-Ape <el.shogun91@gmail.com>
2024-01-06 19:04:14 +0100

    💄 F08: AddForm > Add submit message style

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -75,8 +75,8 @@ export default function AddForm() {
         <Button className="submit-button" label={"Ajouter un nouveau produit au menu"} version="success" />
         {isSubmitted &&
         <div className="submit-message">
-          <FiCheck/>
-          <span>Ajouté avec succès !</span>
+          <FiCheck className="icon"/>
+          <span className="message">Ajouté avec succès !</span>
         </div>
         }
       </div>
@@ -133,7 +133,30 @@ const AddFormStyled = styled.form`
     top: 3px;
 
     .submit-button {
-      height: 100%;
+      width: 50%;
+    }
+
+    .submit-message {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 5px;
+
+      .icon {
+        color: ${theme.colors.success};
+        margin-left: 10px;
+        width: 1em;
+        height: 1em;
+        border: 1px solid ${theme.colors.success};
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+
+      .message {
+        margin-left: 5px;
+        font-size: ${theme.fonts.size.SM};
+        color: ${theme.colors.success};
+      }
     }
   }
 `
