c01065cdd8e7ac8c4a6e705ab66e8a89bf65ff9d
Eratik-Ape <el.shogun91@gmail.com>
2024-01-17 19:20:32 +0100

    ♻️ F10: BASKET > Add Footer

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -3,13 +3,14 @@ import { theme } from './../../../../../theme/index';
 import Header from "../../../reusable-ui/Header";
 import Total from "./Total";
 import { formatPrice } from './../../../../../utils/maths';
+import Footer from "./Footer";
 
 export default function Basket() {
   return (
     <BasketStyled>
       <Total amountToPay={formatPrice(0)} />
       <div className="body">Body</div>
-      <Header>Footer</Header>
+      <Footer />
     </BasketStyled>
   )
 }
