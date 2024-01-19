ba14dbfb8befd5257123f58d77119a842cd62434
Eratik-Ape <el.shogun91@gmail.com>
2024-01-19 19:34:11 +0100

    ♻️ F11: BASKET > Layout > Render BasketProducts conditionally

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -5,14 +5,17 @@ import Footer from "./Footer";
 import { useContext } from "react";
 import OrderContext from "../../../../../context/OrderContext";
 import EmptyBasket from "./EmptyBasket";
+import BasketProducts from "./BasketProducts";
 
 export default function Basket() {
   const {basket} = useContext(OrderContext)
 
+   const isBasketEmpty = basket.length === 0
+
   return (
     <BasketStyled>
       <Total amountToPay={formatPrice(0)} />
-      <EmptyBasket basket={basket} />
+      {isBasketEmpty ? <EmptyBasket /> : <BasketProducts />}
       <Footer />
     </BasketStyled>
   )
