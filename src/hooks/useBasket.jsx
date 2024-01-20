b6fb33288002612ce919a56ebe9d9bbdc606c7b0
Eratik-Ape <el.shogun91@gmail.com>
2024-01-20 21:51:59 +0100

    ♻️ F11: BASKET > Add product > Add sumToPay

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -3,31 +3,35 @@ import {fakeBasket} from "../fakeData/fakeBasket"
 import { deepClone, find, findIndex } from './../utils/array'
 
 export const useBasket = () => { 
-    const [basket, setBasket] = useState(fakeBasket.SMALL)
+    const [basket, setBasket] = useState(fakeBasket.EMPTY)
 
     const handleAddToBasket = (productToAdd) => { 
        const basketCopy = deepClone(basket)
        const isProductAlreadyInBasket = find(productToAdd.id, basketCopy) !== undefined
 
        if(!isProductAlreadyInBasket) {
-        const newBasketProduct = {
-         ...productToAdd,
-         quantity: 1,
-       };
-       const basketUpdated = [newBasketProduct, ...basketCopy]
-       setBasket(basketUpdated)
+        createNewProductInBasket(productToAdd, basketCopy, setBasket)
        return
        }
 
-        incrementProductAlreadyInBasket(productToAdd, basketCopy, setBasket);
+        incrementProductAlreadyInBasket(productToAdd, basketCopy);
       }
 
-      const incrementProductAlreadyInBasket = (productToAdd, basketCopy, setBasket) => {
+      const incrementProductAlreadyInBasket = (productToAdd, basketCopy) => {
         const indexOfBasketProductToIncrement = findIndex(productToAdd.id, basketCopy)
         basketCopy[indexOfBasketProductToIncrement].quantity += 1
         setBasket(basketCopy)
      }
 
+     const createNewProductInBasket = (productToAdd, basketCopy, setBasket) => {
+       const newBasketProduct = {
+         ...productToAdd,
+         quantity: 1,
+       }
+       const basketUpdated = [newBasketProduct, ...basketCopy]
+       setBasket(basketUpdated)
+     }
     return {basket, handleAddToBasket}
  }
+
 
